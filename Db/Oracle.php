37677fa6d58ad95e7b30cd0936926b5066a7c21d
li <1016371613@qq.com>
2016-10-08 14:50:39 +0800

    My  Oracle

--- Db/Oracle.php
+++ Db/Oracle.php
@@ -7,16 +7,19 @@ class Oracle implements IDatabase
     function connect()
     {
         // TODO: Implement connect() method.
+        echo "connect";
     }
 
     function query()
     {
         // TODO: Implement query() method.
+        echo "query";
     }
 
     function close()
     {
         // TODO: Implement close() method.
+        echo "close";
     }
 
 }
