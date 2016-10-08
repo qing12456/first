78f633af8880c6fa260bbef2ff9df041377ec5a0
chen <1056356544@qq.com>
2016-10-08 14:26:33 +0800

    jian

--- Db/Mysql.php
+++ Db/Mysql.php
@@ -6,11 +6,13 @@ class Mysql implements IDatabase
     function connect()
     {
         // TODO: Implement connect() method.
+        echo "小黎是s";
     }
 
     function query()
     {
         // TODO: Implement query() method.
+        echo "小黎是b";
     }
 
     function close()
