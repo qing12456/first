440c93984da612325901b6db82b2e5bad6a23788
HY <https://github.com/07191216/cai.git>
2016-10-08 14:36:31 +0800

    cai

--- Db/Mysqli.php
+++ Db/Mysqli.php
@@ -6,11 +6,13 @@ class Mysqli implements IDatabase
     function connect()
     {
         // TODO: Implement connect() method.
+        echo 'heheheheheh';
     }
 
     function query()
     {
         // TODO: Implement query() method.
+        echo 'hahahahahah';
     }
 
     function close()
