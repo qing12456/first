9f0a3bdbde93a06f9c18c1c08be6bcb4b4467a9c
li <1016371613@qq.com>
2016-10-08 14:05:31 +0800

    My   Mysql

--- Db/Mysql.php
+++ Db/Mysql.php
@@ -5,16 +5,16 @@ class Mysql implements IDatabase
 {
     function connect()
     {
-        // TODO: Implement connect() method.
+        echo "connect";
     }
 
     function query()
     {
-        // TODO: Implement query() method.
+        echo "query";
     }
 
     function close()
     {
-        // TODO: Implement close() method.
+        echo "close";
     }
 }
